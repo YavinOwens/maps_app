e966e8fccf3a4ff36646935e6031d00272f39ec2
Yavin Owens - Developer <88003958+YavinOwens@users.noreply.github.com>
2024-05-12 16:51:32 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -1,27 +1,77 @@
 import streamlit as st 
 import pandas as pd
-import plotly as px
+import plotly.express as px
 
 
-
+st.cache_data()
 def load_data(data):
     return pd.read_csv(data)
 
 def main():
 
     st.title("maps app")
-    menu = ["Home", "Advanced", "Attendees", "About"]
+    menu = ["Home", "Country", "City", "Attendees", "About"]
 
     choice = st.sidebar.selectbox("Menu", menu)
-
+    color = st.sidebar.color_picker("color",value="#9E1FA2")
     if choice == "Home":
         world = load_data("worldcities.csv")
         with st.expander("Data View"):
             st.dataframe(world)
+        fig = px.scatter_mapbox
+        fig = px.scatter_mapbox(world,
+                                lat="lat"
+                                ,lon="lng"
+                                ,hover_name="city"
+                                ,hover_data=["country","population"]
+                                ,color_discrete_sequence=[color]
+                                ,zoom=1
+                                ,height=700)
+        fig.update_layout(mapbox_style="open-street-map")
+        st.plotly_chart(fig)
 
-    fig = px.
-    if choice == "Advanced":
-        pass
+    if choice == "Country":
+        world = load_data("worldcities.csv")
+        countries_list = world["country"].unique().tolist()
+        
+        selected_country = st.sidebar.selectbox("country", countries_list)
+
+        with st.expander("Data View"):
+                df = world[world["country"] == selected_country]
+                st.dataframe(df)
+                fig = px.scatter_mapbox
+                fig = px.scatter_mapbox(df,
+                                lat="lat"
+                                ,lon="lng"
+                                ,hover_name="city"
+                                ,hover_data=["country","population"]
+                                ,color_discrete_sequence=[color]
+                                ,zoom=1
+                                ,height=700)
+        fig.update_layout(mapbox_style="open-street-map")
+        st.plotly_chart(fig)
+
+    if choice == "City":
+        world = load_data("worldcities.csv")
+        countries_list = world["city"].unique().tolist()
+        
+        selected_city = st.multiselect("city", countries_list, default="Jamaica")
+
+        with st.expander("Data View"):
+                df = world[world["city"].isin(selected_city)]
+                st.dataframe(df)
+                fig = px.scatter_mapbox
+                fig = px.scatter_mapbox(df,
+                                lat="lat"
+                                ,lon="lng"
+                                ,hover_name="city"
+                                ,hover_data=["country", "city", "population"]
+                                ,color_discrete_sequence=[color]
+                                ,zoom=1
+                                ,height=700)
+        fig.update_layout(mapbox_style="open-street-map")
+        st.plotly_chart(fig)
+
     if choice == "Attendees":
         pass
     if choice == "About":
